b2b78db18ea0dbd2124083d0325396b929aa27bb
pratik <pratikbhandurge2222@gmail.com>
2022-08-01 12:58:17 +0530

    CoinFlip

--- BasicCoreProgramming/BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/BasicCoreProgramming/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BasicCoreProgramming
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CoinFlip f = new CoinFlip();
+            f.Flip();
+        }
+
+    }
+}
